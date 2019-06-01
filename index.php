ce0036a3bb74104a1552c52fa88833ed084c0ca4
mascolo99 <mascolo99@gmail.com>
2019-06-01 18:06:59 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php echo '<p>Hello World</p>'; $hostname = gethostname(); echo $hostname?> 
  </body>
 </html>
